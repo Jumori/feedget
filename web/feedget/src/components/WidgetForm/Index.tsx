4e32bce8add855acbaad1875e3cbd42ad0557d0d
Juliana Morikoshi <julianamorikoshi@gmail.com>
2022-05-04 00:58:50 -0300

    fea: WidgetForm main popover

---
+++ web/feedget/src/components/WidgetForm/Index.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+
+import { FeedbackTypeStep } from './Steps/FeedbackTypeStep'
+import { FeedbackContentStep } from './Steps/FeedbackContentStep'
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
+
+export const feedbackTypes = {
+  bug: {
+    title: 'Problema',
+    image: {
+      src: bugImageUrl,
+      alt: 'Imagem de um inseto'
+    }
+  },
+  idea: {
+    title: 'Ideia',
+    image: {
+      src: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada'
+    }
+  },
+  other: {
+    title: 'Outro',
+    image: {
+      src: thoughtImageUrl,
+      alt: 'Imagem de um balão de pensamento'
+    }
+  }
+}
+
+export type FeedbackType = keyof typeof feedbackTypes
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+  const handleRestartFeedback = () => {
+    setFeedbackType(null)
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <FeedbackContentStep
+          feedbackType={feedbackType}
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ pela{' '}
+        <a
+          target="_blank"
+          className="underline underline-offset-2"
+          href="https://www.rocketseat.com.br/"
+          rel="noreferrer noopener"
+        >
+          Rocketseat
+        </a>
+      </footer>
+    </div>
+  )
+}
