f9b526ca18d60f503e1747a47e36a6373dec4e21
Juliana Morikoshi <julianamorikoshi@gmail.com>
2022-05-10 18:49:24 -0300

    chore: frontend web folder

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import { BASE_URL } from 'react-native-dotenv'
 
-console.log(process.env.REACT_APP_BASE_URL)
+console.log(process.env)
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL
+  baseURL: process.env.BASE_URL
 })
